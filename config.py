1613c83d91dcbfbb154ef70bcc375fec2e0f8199
Andy Giefer <andreas.giefer@dw.com>
2025-02-25 11:58:03 +0100

    added analysis import to DB

--- config.py
+++ config.py
@@ -2,3 +2,6 @@
 # file is stored here: /Users/giefera/Library/CloudStorage/OneDrive-DeutscheWelle/00\ Reco\ Projects/05\ AI-CODE/2025-02-24\ ATC\ Hate\ Speech\ Detection\
 csvFilename = "atc-bluesky-dataset-hatespeech-analysis.csv"
 
+# database
+databaseName = "atc-analysis.db"
+analysisTableName = "Analysis"
--- database.py
+++ database.py
@@ -1,7 +1,51 @@
 import pandas as pd
 import json
-import config
 from collections import namedtuple
+import sqlite3
+import sys
+import config
+
+
+
+def connect2Database():
+    connection = sqlite3.connect(config.databaseName, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    return connection
+
+def dropDatabase():
+    
+    connection = connect2Database()
+    cursor = connection.cursor()
+
+    query = f"DROP TABLE IF EXISTS {config.analysisTableName}"
+    cursor.execute(query)
+
+# creates database with all tables
+def createDatabase():
+
+    connection = connect2Database()
+    cursor = connection.cursor()
+
+    # create posts table
+    query = f"""
+        CREATE TABLE IF NOT EXISTS {config.analysisTableName} 
+            (
+                id STRING NOT NULL UNIQUE,
+                author STRING, 
+                creationDate TIMESTAMP, 
+                language STRING,
+                text TEXT,
+                nonHateful FLOAT,
+                racism FLOAT,
+                religious FLOAT,
+                sexism FLOAT,
+                sexualOrientation FLOAT
+            )
+    """ 
+    cursor.execute(query)
+
+    # create index
+    #cursor.execute(f"CREATE INDEX idx_addedToDB ON {config.analysisTableName}(addedToDB)")
+
 
 def extractPrediction(analysisJSON):
 
@@ -28,21 +72,49 @@ def extractPrediction(analysisJSON):
     # return result
     return prediction
 
+def addAnalysisRow(id, author, creationDate, language, text, nonHateful, racism, religious, sexism, sexualOrientation):
+
+    connection = connect2Database()
+    cursor = connection.cursor()
+
+    query = f"""INSERT OR IGNORE INTO {config.analysisTableName} (id, author, creationDate, language, text, nonHateful, racism, religious, sexism, sexualOrientation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+    
+    try:
+        valueTuple = (id, author, creationDate, language, text, nonHateful, racism, religious, sexism, sexualOrientation)
+        cursor.execute(query, valueTuple)
+        connection.commit()
+    
+    except sqlite3.InterfaceError as e:
+        print("Error while adding analysis point")
+        print(e)
+        print(f"query: {query}")
+        print(valueTuple)
+        
+    connection.close()
+
+
 def importCSV2DB():
     
     # making data frame from csv file
     data = pd.read_csv(config.csvFilename)
 
     for tuple in data.itertuples():
-        (index, id, author, creationDate, webUrl, text, analysisJSON) = tuple
+        (index, id, author, creationDate, language, text, analysisJSON) = tuple
 
         p = extractPrediction(analysisJSON)
-        print(p.sexism)
+        
+        addAnalysisRow(id=id, author=author, creationDate=creationDate, language=language, text=text,
+                       nonHateful=p.nonHateful, racism=p.racism, religious=p.religious, sexism=p.sexism, sexualOrientation=p.sexualOrientation)
 
-        break
+        sys.stdout.write(f"\r{index}")
+        sys.stdout.flush()
+
+        #break
 
 if __name__ == '__main__':
     
-    importCSV2DB()
+    #dropDatabase()
+    #createDatabase()
+    #importCSV2DB()
 
 
