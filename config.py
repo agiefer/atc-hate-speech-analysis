43335b6bf7614f2c833d51c5f9bfa377a2e07d59
Andy Giefer <andreas.giefer@dw.com>
2025-02-25 10:38:35 +0100

    added comment to show where CSV data file is

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-
+# file is stored here: /Users/giefera/Library/CloudStorage/OneDrive-DeutscheWelle/00\ Reco\ Projects/05\ AI-CODE/2025-02-24\ ATC\ Hate\ Speech\ Detection\
 csvFilename = "atc-bluesky-dataset-hatespeech-analysis.csv"
 
